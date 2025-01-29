cb41ef1a9ab74f72b7d9cce026ff7484bde8e13d
John Schultz <john.schultz@berkeley.edu>
2025-01-28 22:58:15 -0800

    added user reports db

--- manage_db.py
+++ manage_db.py
@@ -5,25 +5,50 @@
 # Locked - I couldn't enter the room - 'locked'
 
 import sqlite3
+import datetime
 
 def create_flag_table():
     con = sqlite3.Connection("./reports.db")
     cur = con.cursor()
 
-    cur.execute("CREATE TABLE reports IF NOT EXISTS (id INTEGER PRIMARY KEY, flag TEXT, location TEXT, datetime TEXT);")
-    cur.close()
+    cur.execute("CREATE TABLE IF NOT EXISTS reports (flag TEXT, location TEXT, datetime TEXT);")
+    con.commit()
     con.close()
 
 def add_report(flag, locationName, dt):
-    use = dt.replace(day=1, month=1, year=1900)
-
     con = sqlite3.Connection("./reports.db")
     cur = con.cursor()
 
-    cur.execute("INSERT INTO reports VALUES ({f}, {l}, {d});".format(f=flag, l=locationName, d=use))
+    cur.execute("INSERT INTO reports VALUES ('{f}', '{l}', '{d}');".format(f=flag, l=locationName, d=use))
 
     cur.close()
+    con.commit()
     con.close()
 
-def 
+def get_report(locationName):
+    con = sqlite3.Connection("./reports.db")
+    cur = con.cursor()
+    
+    cur.execute("SELECT * FROM reports WHERE location = '{l}'".format(l=locationName))
 
+    out = cur.fetchall()
+    con.commit()
+    con.close()
+    return out
+
+def get_all_reports():
+    con = sqlite3.Connection("./reports.db")
+    cur = con.cursor()
+
+    cur.execute("SELECT * FROM reports")
+
+    out = cur.fetchall()
+    con.commit()
+    con.close()
+
+    return out
+
+def dtrp_to_dt(dtrp):
+    form = "%Y-%m-%d %H:%M:%S.%f"
+
+    return datetime.datetime.strptime(dtrp, form)
